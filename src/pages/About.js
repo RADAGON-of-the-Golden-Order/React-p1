b6ee479e139b41e73c54522cec8f86cfb5db433d
RADAGON-of-the-Golden-Order <nelsonmarimoutou10@gmail.com>
2023-11-17 21:40:00 +0400

    Première version

---
+++ src/pages/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At ea atque
+        perspiciatis neque corrupti distinctio, cupiditate vitae quia est esse
+        sapiente mollitia consectetur eveniet optio necessitatibus voluptas ad
+        possimus et ducimus culpa, dolorem temporibus. Qui quasi esse nulla
+        reprehenderit porro eveniet maiores rem harum corporis! Quasi inventore
+        voluptate, soluta beatae quas eum tempore saepe laborum fugit voluptatum
+        quaerat maiores cum exercitationem? Vel id autem, quaerat molestiae,
+        asperiores praesentium hic ducimus quis veniam suscipit nesciunt quos,
+        officia modi dolorem mollitia nemo odit? Adipisci officia voluptatem
+        libero necessitatibus, eveniet repellendus, earum officiis neque sed
+        dignissimos, quidem deserunt veniam nihil atque! Accusamus, similique?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+        ducimus nostrum nulla reprehenderit qui perspiciatis repudiandae soluta,
+        voluptate sed maxime odio dolore quam reiciendis corporis, quas ullam,
+        molestias exercitationem libero nobis quaerat et beatae culpa? Pariatur
+        qui quidem nesciunt rem magnam est suscipit, ab optio, tempora odio
+        dolor dolorem, et saepe necessitatibus enim atque ullam iusto vel?
+        Accusantium nisi dolorum voluptatibus, ipsam vel non cum voluptas ab
+        soluta magnam velit aliquid incidunt aspernatur doloremque molestias
+        sapiente adipisci omnis vitae labore reiciendis animi sint praesentium
+        minima! Fugit dolorum porro voluptatum? Molestias est obcaecati
+        similique impedit explicabo quibusdam laudantium provident, iste esse?
+        Dicta quibusdam distinctio saepe doloribus error neque iste aspernatur
+        alias earum aperiam quis officia, odio iure voluptatem ducimus?
+        Consequuntur, deserunt! Rem fugiat harum dolorum quos ad et veniam
+        libero, consequatur, nesciunt voluptatem deserunt eius delectus aliquid
+        iure molestias. Voluptate, voluptatum impedit aperiam natus quaerat,
+        delectus laudantium magni quibusdam quia distinctio blanditiis
+        perspiciatis soluta. Tempore, reprehenderit? Modi eligendi nam dicta
+        saepe eos doloribus voluptates labore distinctio debitis, qui cumque
+        quos natus ducimus aperiam eaque dignissimos aspernatur amet consequatur
+        illum architecto sapiente at. Voluptas, eligendi provident? Nihil
+        sapiente dolorem voluptates vel dicta alias, unde ipsum est,
+        exercitationem, voluptatibus cupiditate voluptatum iure necessitatibus!
+      </p>
+    </div>
+  );
+};
+
+export default About;
